aa5645fcfaa2ee991f4fc45c0e81aae45d56a1d8
Zahra-shadmani <zahradarya09@gmail.com>
2025-01-09 17:33:12 +0330

    define cancle order page

--- app/cancle/page.tsx
+++ app/cancle/page.tsx
@@ -1,12 +1,9 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const OrderCancellation: React.FC = () => {
-
-
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
       <img
@@ -14,15 +11,18 @@ const OrderCancellation: React.FC = () => {
         alt="Order Cancellation"
         className=" w-64 mb-6"
       />
-      <h1 className="text-2xl font-semibold text-red-800 mb-4">سفارش شما کنسل شد</h1>
+      <h1 className="text-2xl font-semibold text-red-800 mb-4">
+        سفارش شما کنسل شد
+      </h1>
       <p className="text-gray-600 mb-6">
-      با عرض پوزش به شما اطلاع می دهیم که سفارش شما لغو شده است      </p>
-     <Link href={'/'}> <button
+        با عرض پوزش به شما اطلاع می دهیم که سفارش شما لغو شده است
+      </p>
+      <Link href={"/"}>
    
-        className="px-4 py-2 bg-green-500 text-white rounded hover:bg-blue-600 transition duration-200"
-      >
-     بازگشت به صفحه اصلی
-      </button></Link>
+        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-blue-600 transition duration-200">
+          بازگشت به صفحه اصلی
+        </button>
+      </Link>
     </div>
   );
 };
