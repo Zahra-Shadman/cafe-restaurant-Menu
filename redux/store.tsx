96e9496cd2cd9a44156d804f39899f186102d35c
Zahra-shadmani <zahradarya09@gmail.com>
2025-01-09 17:30:46 +0330

    little chenge

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,38 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { 
-  persistStore, 
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
   persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER 
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import cartReducer from './slices/cartSlice';
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+import cartReducer from "./slices/cartSlice";
 
-// Persist configuration
 const persistConfig = {
-  key: 'root',
+  key: "root",
   version: 1,
   storage,
 };
 
-// persistedReducer
 const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
   reducer: {
-    cart: persistedReducer
+    cart: persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      }
-    })
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
